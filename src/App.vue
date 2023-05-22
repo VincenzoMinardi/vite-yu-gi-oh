443e4cf8768f69a616ba251bc2fb0bbddfca296b
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-22 15:03:56 +0200

    add: vue and vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import CardHeader from './components/CardHeader.vue';
+import CardMain from './components/CardMain.vue';
+
+export default{
+  components: {
+    CardHeader,
+    CardMain,
+  }
+}
+ 
+
+</script>
+
+<template>
+  <CardHeader />
+  <CardMain />
+</template>
+
+<style>
+
+</style>
