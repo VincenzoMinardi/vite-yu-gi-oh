2d5aec54e99baa50a7eb9c8751e14503989262c0
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-24 12:29:24 +0200

    add: fontawesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
 createApp(App).mount('#app')
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
